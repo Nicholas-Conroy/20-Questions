e7748ea275e8aaaec722d9079ec1e522ca1037ce
Nicholas-Conroy <nsconroy@hotmail.com>
2024-02-12 12:04:54 -0500

    flasking it up

--- testapp.py
+++ testapp.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__) # __name__ refers to this file (will be equal to __main__ if file is run from itself, if imported then it is name of file)
 
-@app.route('/') #route for app to follow
-
+@app.route('/') #route for app to follow, this triggers the following function
 def index():
      #knows to check templates folder, just specify file to render
     return render_template("index2.html")
